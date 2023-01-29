1a2c9749ebfb2a99dcb2ddbc019c66bc478b79a8
Ben Aaron <ben@theaarons.com>
2023-01-29 14:54:19 -0800

    fetch metadata collection if it exists

--- api/app/database.py
+++ api/app/database.py
@@ -2,6 +2,7 @@ import os
 import pymilvus 
 
 MILVUS_ALIAS = os.environ.get('MILVUS_ALIAS', 'default')
+MODEL_META_COLLECTION_NAME = "meta.models"
 
 connection = pymilvus.connections.connect(
   alias=MILVUS_ALIAS,
@@ -11,25 +12,25 @@ connection = pymilvus.connections.connect(
 
 def create_or_get_model_metadata():
     """Create a new collection for an index of language models"""
-    model_name = pymilvus.FieldSchema(
-        name="book_name", 
-        dtype=pymilvus.DataType.VARCHAR, 
-        max_length=256,
-    )
-    model_num_dims = pymilvus.FieldSchema(
-        name="num_dims",
-        dtype=pymilvus.DataType.INT64,
-    )
-    schema = pymilvus.CollectionSchema(
-        fields=[model_name, model_num_dims], 
-        description="Test book search"
-    )
-    try:
+    if pymilvus.utility.has_collection(MODEL_META_COLLECTION_NAME, using=MILVUS_ALIAS):
+        return pymilvus.Collection(name=MODEL_META_COLLECTION_NAME, using=MILVUS_ALIAS)
+    else:
+        model_name = pymilvus.FieldSchema(
+            name="book_name", 
+            dtype=pymilvus.DataType.VARCHAR, 
+            max_length=256,
+        )
+        model_num_dims = pymilvus.FieldSchema(
+            name="num_dims",
+            dtype=pymilvus.DataType.INT64,
+        )
+        schema = pymilvus.CollectionSchema(
+            fields=[model_name, model_num_dims], 
+            description="Test book search"
+        )
         collection = pymilvus.Collection(
-            name="meta.models",
+            name=MODEL_META_COLLECTION_NAME,
             schema=schema,
             using=MILVUS_ALIAS,
         )
-    except Exception as exc:
-        raise exc
-    return collection
+        return collection
