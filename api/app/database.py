1387770a72c859719f42c768971e2a47a4453799
Ben Aaron <ben@theaarons.com>
2023-01-29 18:52:11 -0800

    paint it black

--- api/app/database.py
+++ api/app/database.py
@@ -1,14 +1,15 @@
 import os
-import pymilvus 
+import pymilvus
 
-MILVUS_ALIAS = os.environ.get('MILVUS_ALIAS', 'default')
+MILVUS_ALIAS = os.environ.get("MILVUS_ALIAS", "default")
 MODEL_META_COLLECTION_NAME = "meta.models"
 
 connection = pymilvus.connections.connect(
-  alias=MILVUS_ALIAS,
-  host=os.environ.get('MILVUS_HOST', 'localhost'),
-  port=os.environ.get('MILVUS_PORT', '19530'),
+    alias=MILVUS_ALIAS,
+    host=os.environ.get("MILVUS_HOST", "localhost"),
+    port=os.environ.get("MILVUS_PORT", "19530"),
 )
+
 
 def create_or_get_model_metadata():
     """Create a new collection for an index of language models"""
@@ -16,8 +17,8 @@ def create_or_get_model_metadata():
         return pymilvus.Collection(name=MODEL_META_COLLECTION_NAME, using=MILVUS_ALIAS)
     else:
         model_name = pymilvus.FieldSchema(
-            name="book_name", 
-            dtype=pymilvus.DataType.VARCHAR, 
+            name="book_name",
+            dtype=pymilvus.DataType.VARCHAR,
             max_length=256,
         )
         model_num_dims = pymilvus.FieldSchema(
@@ -25,8 +26,7 @@ def create_or_get_model_metadata():
             dtype=pymilvus.DataType.INT64,
         )
         schema = pymilvus.CollectionSchema(
-            fields=[model_name, model_num_dims], 
-            description="Test book search"
+            fields=[model_name, model_num_dims], description="Test book search"
         )
         collection = pymilvus.Collection(
             name=MODEL_META_COLLECTION_NAME,
